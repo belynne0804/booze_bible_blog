8e3eddada3d1ed5b3755350c059d52cc353f92cf
Amy Dixon <amy.dixon@rocky.edu>
2021-07-05 13:00:21 -0600

    uninstall vetur, moved files around to accomodate new vue router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Main from '../views/Main.vue'
 
 export default {
-  name: 'Home',
+  name: 'Main',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
